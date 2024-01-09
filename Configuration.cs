df7595b2c1a4a6b15892675abc64b2fb34752b5d
Dener Niels <ksin_niels@outlook.com>
2024-01-09 07:35:52 +0000

    Criação de tokens Jwt

---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        // TOKEN - JWT - Jason Web Token
+        public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+
+    }
+}
